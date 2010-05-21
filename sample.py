db836995ffd9896486b8edade1c292cf92bf0260
Noufal Ibrahim <noufal@nibrahim.net.in>
2010-05-22 04:20:40 +0530

    Blocking problem on read

--- sample.py
+++ sample.py
@@ -2,60 +2,81 @@
 
 import select
 import socket
-import threading
-import SocketServer
 
-class HTTPDebugHandler(SocketServer.StreamRequestHandler):
-    """
-    The RequestHandler class for our server.
-
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
-    def local_to_remote(self):
-        while True:
-            r, w, x = None, None, None
-            r, w, x = select.select([self.rfile],[],[])
-            if r:
-                data = self.rfile.readline()
-                print "> %s"%data
-                self.server.rsock.write(data)
-            
-    def remote_to_local(self):
-        while True:
-            r, w, x = None, None, None
-            r, w, x = select.select([self.server.rsock],[],[])
-            if r:
-                data = self.server.rsock.readline()
-                print "< %s"%data
-                self.wfile.write(data)
+def printlist(data, prefix):
+    "Simple printer with a prefix"
+    for i in data:
+        print prefix, i
     
-    def handle(self):
-        print "Received connection"
-        l2r = threading.Thread(group = None, target = self.local_to_remote)
-        r2l = threading.Thread(group = None, target = self.remote_to_local)
-        l2r.start()
-        r2l.start()
-        l2r.join()
 
+def read_request(sock):
+    "Read an HTTP request from the local socket"
+    retval = []
+    while True:
+        line = sock.readline()
+        retval.append(line)
+        if line == "\r\n":
+            return retval
 
-class HTTPDebug(SocketServer.TCPServer):
-    def __init__(self, local_addr, remote_addr):
-        self._rsock = socket.socket()
-        self._rsock.connect(remote_addr)
-        self.rsock = self._rsock.makefile("r+")
-        SocketServer.TCPServer.__init__(self, local_addr, HTTPDebugHandler)
+def send_request(request, sock):
+    "Sends the request to the remote end"
+    for i in request:
+        sock.send(i)
+
+def read_response(sock):
+    retval = []
+    while True:
+        # r,w,x = None, None, None
+        # print "selecting"
+        # r,w,x, = select.select([sock],[],[],5)
+        # print "selected"
+        # if r:
+        line = sock.readline()
+        print "'%s'"%line.strip()
+        retval.append(line)
+        r,w,x = None, None, None
+        print "selecting"
+        r,w,x, = select.select([sock],[],[],5)
+        print "selected"
+        print r,w,x
+        # else:
+        #     break
+    return retval
+
+def send_response(response, sock):
+    for i in response:
+        sock.send(i)
+        
+def run_server(lsock, rsock):
+    lsock.listen(1)
+    conn,addr = lsock.accept()
+    # conn.setblocking(False)
+    # rsock.setblocking(False)
+    ls_file = conn.makefile("r+")
+    while True:
+        request = read_request(ls_file)
+        printlist(request, "> ")
+        send_request(request, rsock)
+        rs_file = rsock.makefile()
+        response = read_response(rs_file)
+        printlist(response, "< ")
+        send_response(response, conn)
+
+def main(lport, remote):
+    lsock = socket.socket()
+    lsock.bind(("localhost", int(lport)))
+    addr = remote.split(":")
+    rsock = socket.socket()
+    rsock.connect((addr[0], int(addr[1])))
+    run_server(lsock, rsock)
+    
+               
+    
 
 
 if __name__ == "__main__":
     import sys
-    HOST, PORT = sys.argv[1:]
-    
-    # Create the server, binding to localhost on port 9999
-    server = HTTPDebug((HOST, int(PORT)), ("www.google.com", 80))
+    lport, remote = sys.argv[1:]
+    sys.exit(main(lport, remote))
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    print "Starting server on %s port %s"%(HOST, PORT)
-    server.serve_forever()
+
